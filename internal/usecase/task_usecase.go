59be5f7227ba17b15afda2abbf3fbb2d47f771e7
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-31 04:43:51 +0300

    feat: added usecase

--- internal/usecase/task_usecase.go
+++ internal/usecase/task_usecase.go
@@ -1,1 +1,47 @@
 package usecase
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Util787/task-processor/internal/common"
+	"github.com/Util787/task-processor/internal/models"
+)
+
+func (u *TaskUsecase) EnqueueTask(ctx context.Context, task models.Task) error {
+	op := common.GetOperationName()
+
+	//validation
+	err := validateTask(task)
+	if err != nil {
+		return fmt.Errorf("%s: %w: %w", op, models.ErrValidation, err)
+	}
+
+	err = u.taskProcessQueue.EnqueueTask(ctx, task)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (u *TaskUsecase) GetTaskState(ctx context.Context, taskID string) (models.TaskState, error) {
+	op := common.GetOperationName()
+
+	state, err := u.taskStorage.GetTaskState(ctx, taskID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	return state, nil
+}
+
+func validateTask(task models.Task) error {
+	if task.Id == "" {
+		return fmt.Errorf("task ID is required")
+	}
+
+	if task.MaxRetries <= 0 {
+		return fmt.Errorf("max retries must be > 0")
+	}
+
+	return nil
+}
--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,1 +1,27 @@
 package usecase
+
+import (
+	"context"
+
+	"github.com/Util787/task-processor/internal/models"
+)
+
+type TaskStateStorage interface {
+	GetTaskState(ctx context.Context, taskID string) (models.TaskState, error)
+}
+
+type TaskProcessQueue interface {
+	EnqueueTask(ctx context.Context, task models.Task) error
+}
+
+type TaskUsecase struct {
+	taskStorage      TaskStateStorage
+	taskProcessQueue TaskProcessQueue
+}
+
+func NewTaskUsecase(taskStorage TaskStateStorage, taskProcessQueue TaskProcessQueue) *TaskUsecase {
+	return &TaskUsecase{
+		taskStorage:      taskStorage,
+		taskProcessQueue: taskProcessQueue,
+	}
+}
