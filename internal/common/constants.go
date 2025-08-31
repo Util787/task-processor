6de0147635ce18897095fe8880b542cd09a177a3
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-31 04:41:45 +0300

    feat: added common funcs and constants

---
+++ internal/common/constants.go
@@ -0,0 +1,4 @@
+package common
+
+// use it to set or get key in context with value
+type ContextKey string
--- internal/common/funcs.go
+++ internal/common/funcs.go
@@ -1,1 +1,16 @@
 package common
+
+import "runtime"
+
+// GetOperationName returns PackageName.FunctionName of the func it was called in
+//
+// it should be used for logging or error wrapping
+func GetOperationName() string {
+
+	function, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return "couldnt get op name"
+	}
+
+	return runtime.FuncForPC(function).Name()
+}
